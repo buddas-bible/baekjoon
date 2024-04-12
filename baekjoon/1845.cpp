409a70501b6f094c587b8311a93de83ac74dd87e
buddas-bible <kh951220@gmail.com>
2024-04-12 23:23:20 +0900

    [정기훈] 임시커밋

---
+++ baekjoon/1845.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+// https://www.acmicpc.net/problem/1845
+
+void Input()
+{
+	std::cin.tie();
+	std::cout.tie();
+	std::ios_base::sync_with_stdio(false);
+
+	freopen("input.txt", "rt", stdin);
+
+}
+
+int main()
+{
+	Input();
+
+	return 0;
+}
